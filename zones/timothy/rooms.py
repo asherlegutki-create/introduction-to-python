c8a98bd8a8faee998b3d30e86e6788258d2b0984
TimothyReagan <timothyreagan10@gmail.com>
2026-06-01 21:34:28 +0000

    added more room templates

--- zones/timothy/rooms.py
+++ zones/timothy/rooms.py
@@ -17,7 +17,387 @@ from . import mobs as M
 ROOMS: dict[int, Room] = {
     1: Room(
         {
-            "number": 1,
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ),
+     2: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 3: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 4: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 5: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 6: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 7: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 8: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 9: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 10: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 11: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 12: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 13: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 14: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 15: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 16: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 17: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 18: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 19: Room(
+        {
+            "name": "&wDark Bramble Woods",
+            "description": "A foggy woods with brambles scattered about.",
+            "indoors": False,
+            "terrain": "grassy",
+            "exits": [
+                {"direction": "north", "roomId": 99005, "external": True},
+                {"direction": "south", "roomId": 1},
+                {"direction": "east", "roomId": 1},
+                {"direction": "west", "roomId": 1},
+            ],
+            "objects": [
+                O.spawn("Brumplin Seed"),
+            ],
+            "mobs": [
+                M.spawn("Brumplin Minor"),
+                M.spawn("Brumplin Alpha")
+            ],  # two independent students
+        }
+    ), 20: Room(
+        {
             "name": "&wDark Bramble Woods",
             "description": "A foggy woods with brambles scattered about.",
             "indoors": False,
